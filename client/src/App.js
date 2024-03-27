bd8dd68cb9a03a41c38825d32dc3d298b284293b
Sakindeb <sakindeborah@gmail.com>
2024-03-27 10:18:15 +0300

    Formed ground work for the routes for pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider} from 'react-router-dom'
+/** Root routes */
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <div>Root route</div>
+    },
+    {
+        path: '/about',
+        element: <div>About us</div>
+    },
+    {
+        path: '/project',
+        element: <div>View projects</div>
+    }
+])
 
 export default function App(){
     return(
-        <div>Create react</div>
+    <main>
+        <RouterProvider router={router}></RouterProvider>
+    </main>
     )
 }
