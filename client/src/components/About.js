b0a0940786871e37a8e0b01ccbec0d8385761b3c
Sakindeb <sakindeborah@gmail.com>
2024-03-30 16:56:20 +0300

    Added some styling to the frontend

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,7 +1,16 @@
 import React from "react";
-
+import Nav from "./Nav";
+import styles from "../styles/About.module.css"
 export default function About(){
     return(
-        <div>About</div>
+       <div>
+        <Nav />
+           <div class={styles.container}>
+		<div class={styles.blur}>
+			<h2> Blurred background image </h2>
+		</div>
+    </div>	
+    </div>
+        
     )
 }
